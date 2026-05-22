63e2e4008844c610f8a2bd16264586ece16f2ef4
Matthieu Riegler <kyro38@gmail.com>
2026-05-22 22:59:03 +0200

    fix(common): add upper bounds for digitsInfo
    The prevents the `roundNumber` function from allocating a large array.
    
    (cherry picked from commit 62dd27d6afe4bfd9d4fa8956c1fa191c9db61dfa)

--- packages/common/test/i18n/format_number_spec.ts
+++ packages/common/test/i18n/format_number_spec.ts
@@ -43,6 +43,18 @@ describe('Format number', () => {
           /is higher than the maximum/,
         );
       });
+
+      it('should throw if minInt, minFraction, or maxFraction exceeds 100 to prevent DoS', () => {
+        const expectedError = /Exceeded maximum limits of 100 digits/;
+        expect(() => formatNumber(1.1, ɵDEFAULT_LOCALE_ID, '101.4-5')).toThrowError(expectedError);
+        expect(() => formatNumber(1.1, ɵDEFAULT_LOCALE_ID, '3.101-105')).toThrowError(
+          expectedError,
+        );
+        expect(() => formatNumber(1.1, ɵDEFAULT_LOCALE_ID, '3.4-101')).toThrowError(expectedError);
+        expect(() => formatNumber(1.1, ɵDEFAULT_LOCALE_ID, '1.2000000000-20000000')).toThrowError(
+          expectedError,
+        );
+      });
     });
 
     describe('transform with custom locales', () => {
